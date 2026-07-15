ac21e763f551de05f0d5815267084fbee0092e07
LukFau <lukas.faupel@gmail.com>
2026-07-15 21:18:21 +0200

    gcloud prep

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig(() => {
       hmr: process.env.DISABLE_HMR !== 'true',
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      allowedHosts: [
+          'bevtally-app-986618385853.europe-west3.run.app' // Füge deine URL hier ein
+      ]
     },
   };
 });
